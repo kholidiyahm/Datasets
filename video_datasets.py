2643d665fd815fd24ab71106d9bb5e75731fab72
kholidiyahm <125634044+kholidiyahm@users.noreply.github.com>
2024-02-10 20:10:27 +0700

    Add files via upload

---
+++ video_datasets.py
@@ -0,0 +1,5 @@
+| Video Name     | Duration |         Link YouTube         |
+|----------------|----------|------------------------------|
+| Road Traffic   |   5:07   | https://youtu.be/289rvo6RQDc |
+| Cell 2,1 | Cell 2,2 | Cell 2,3 |
+| Cell 3,1 | Cell 3,2 | Cell 3,3 |
